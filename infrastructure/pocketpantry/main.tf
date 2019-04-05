3bfbe58fc483398f5a1151ea24874fcc5f91b4c0
James Bannan <james@bannan.com.au>
2019-04-05 22:26:19 +1100

    Corrected reference to local

--- infrastructure/pocketpantry/main.tf
+++ infrastructure/pocketpantry/main.tf
@@ -7,7 +7,7 @@ provider "azuread" {
 data "azurerm_client_config" "current" {}
 module "resource-group" {
   source                  = "../modules/resource-group"
-  resource-group_name     = "${local.solution-short-name-lower}"
+  resource-group_name     = "${local.solution-name-lower}"
   resource-group_location = "${var.primary_region}"
   resource-group_tags = {
     solution    = "${local.solution-name}"
