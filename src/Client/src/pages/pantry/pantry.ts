30630831a181119fffbe3b6fa4e104220a85546f
Ollie Flavel <ollie.flavel@insight.com>
2019-04-06 18:08:58 +1100

    Inital work

--- src/Client/src/pages/pantry/pantry.ts
+++ src/Client/src/pages/pantry/pantry.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 /**
  * Generated class for the PantryPage page.
@@ -14,12 +15,32 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'pantry.html',
 })
 export class PantryPage {
+  currentImage: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private camera: Camera) {
   }
-
+  
   ionViewDidLoad() {
     console.log('ionViewDidLoad PantryPage');
+  }
+
+  uploadReceipt() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.currentImage = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+     console.log("Camera issue:" + err);
+    });
   }
 
 }
