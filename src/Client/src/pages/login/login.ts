6bef67517d75796d905f0daaffcb3231ce01a0f5
Emlyn.Rice <emlyn.rice@ignia.com.au>
2019-04-18 15:54:27 +0800

    Facebook installed but not inislizing :) Maybe upgrade?

--- src/Client/src/pages/login/login.ts
+++ src/Client/src/pages/login/login.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { IonicPage, NavController, ToastController } from 'ionic-angular';
-
-import { User } from '../../providers';
+import { IonicPage, NavController, ToastController, LoadingController } from 'ionic-angular';
 import { MainPage } from '../';
+import { FacebookAuth } from '../../providers/auth/FacebookAuth';
 
 @IonicPage()
 @Component({
@@ -11,40 +10,37 @@ import { MainPage } from '../';
   templateUrl: 'login.html'
 })
 export class LoginPage {
-  // The account fields for the login form.
-  // If you're using the username field with or without email, make
-  // sure to add it to the type
-  account: { email: string, password: string } = {
-    email: 'test@example.com',
-    password: 'test'
-  };
-
-  // Our translated text strings
-  private loginErrorString: string;
 
   constructor(public navCtrl: NavController,
-    public user: User,
+    private facebookAuth: FacebookAuth,
     public toastCtrl: ToastController,
-    public translateService: TranslateService) {
+		public loadingController: LoadingController,
+    public translateService: TranslateService) {}
 
-    this.translateService.get('LOGIN_ERROR').subscribe((value) => {
-      this.loginErrorString = value;
-    })
-  }
-
-  // Attempt to login in through our User service
-  doLogin() {
-    this.user.login(this.account).subscribe((resp) => {
-      this.navCtrl.push(MainPage);
-    }, (err) => {
-      this.navCtrl.push(MainPage);
-      // Unable to log in
-      let toast = this.toastCtrl.create({
-        message: this.loginErrorString,
-        duration: 3000,
-        position: 'top'
-      });
-      toast.present();
+  async doFaceBookLogin(){
+		const loading = await this.loadingController.create({
+			content: 'Please wait...'
     });
-  }
+    
+    this.presentLoading(loading);
+
+		this.facebookAuth.login()
+      .then(() =>{
+        this.navCtrl.push(MainPage);;
+        loading.dismiss();
+      }, error =>{
+        console.log(error);
+        let toast = this.toastCtrl.create({
+          message: error,
+          duration: 3000,
+          position: 'top'
+        });
+        toast.present();
+        loading.dismiss();
+      })
+	}
+
+	async presentLoading(loading) {
+		return await loading.present();
+	}
 }
